0b9c3eb399a42db0c2aaa83f1330e6eff00620f8
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-09-08 22:21:20 +0300

    test server with http module

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-// //OPEN SERVER WITH 'HTTP' MODULE
-// const host = 'localhost';
-// const port = 8000;
+//OPEN SERVER WITH 'HTTP' MODULE
+const http = require('http');
 
-// const requireListener = function (req, res) {
-//   res.writeHead(200);
-//   res.end("Hello node");
-// };
+const host = 'localhost';
+const port = 8000;
 
-// const server = http.createServer(requireListener)
-// server.listen(port, host, () => {
-//   console.log(`Server is running on http://${host}:${port}`)
-// });
+const requireListener = function (req, res) {
+  res.writeHead(200);
+  res.end("Hello node");
+};
+
+const server = http.createServer(requireListener)
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`)
+});
 
 
 // //CODE FOR AWS LAMBDA
@@ -23,12 +25,11 @@
 // };
 
 
-//GENERATE HTML FROM SERVER
-var http = require('http');
+// //GENERATE HTML FROM SERVER
+// var http = require('http');
  
-http.createServer(function(request, respone){
-  respone.writeHead(200, {'Content-type':'text/plan'});
-  response.write('Hello Node JS Server Response');
-  response.end( );
+// http.createServer(function(request, respone){
+//   respone.writeHead(200, {'Content-type':'text/plan'});
+//   response.end("Hello node");
  
-}).listen(7000);
+// }).listen(8000);
