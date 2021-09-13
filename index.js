e1b9a986b5c6d68351a2d324349152a504a1406b
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-09-13 22:28:47 +0300

    testing deploying to AWS Lambda

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 exports.handler = async (event) => {
   const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello Alina!'),
+      body: JSON.stringify('Hello AWS!'),
   };
   return response;
 };
